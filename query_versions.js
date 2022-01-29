dde55e13bd57def17662ddcdabb532bada0b1fa6
Meno Abels <meno.abels@adviser.com>
2022-01-29 17:23:31 +0000

    chore: added redirect handling on query.js

--- query_versions.js
+++ query_versions.js
@@ -2,8 +2,7 @@ const https = require('https');
 
 const APIUSER = process.env.APIUSER && process.env.APIUSER.length ? process.env.APIUSER : undefined;
 
-async function latestRelease(repo, page, list, cb) {
-  const url = `/repos/${repo}/releases/latest`;
+async function latestRelease(repo, url, page, list, cb) {
   https.get({
   hostname: `api.github.com`,
   port: 443,
@@ -19,6 +18,11 @@ async function latestRelease(repo, page, list, cb) {
   path: url,
   method: 'GET'
 }, (res) => {
+	if (300 < ~~res.statusCode && ~~res.statusCode < 400) {
+		// console.log(res.headers['location']);
+		latestRelease(repo, res.headers['location'])
+		return
+	}
 	if (!(200 <= ~~res.statusCode && ~~res.statusCode < 300)) {
         	console.error('statusCode:', url, res.statusCode);
 		process.exit(1);
@@ -28,7 +32,7 @@ async function latestRelease(repo, page, list, cb) {
 		out += d;
 	});
 	res.on('end', function (chunk) {
-	    // console.log("# ", repo);
+	    // console.log("# ", url);
 	    console.log(`${repo.toUpperCase().replace(/[^A-Z0-9]/g, '_')}_VERSION=${JSON.parse(out).tag_name}`);
 		/*
 	    const json = JSON.parse(out);
@@ -48,8 +52,12 @@ async function latestRelease(repo, page, list, cb) {
 }
 
 process.argv.slice(2).forEach(repo => {
-	latestRelease(repo, versions => {
+  	const url = `/repos/${repo}/releases/latest`;
+	latestRelease(repo, url);
+	/*
+		, versions => {
 		console.log(versions);
 	});
+	*/
 })
 
