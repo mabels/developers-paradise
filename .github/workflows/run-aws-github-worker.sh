66856caad9e786bb89dfdf19fb7ffc06847e1bb8
Meno Abels <meno.abels@adviser.com>
2025-11-23 13:51:58 +0100

    chore: fix here doc

--- .github/workflows/run-aws-github-worker.sh
+++ .github/workflows/run-aws-github-worker.sh
@@ -97,13 +97,13 @@ apt install -y ca-certificates curl jq awscli
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
-tee /etc/apt/sources.list.d/docker.sources <<EOF
+tee /etc/apt/sources.list.d/docker.sources <<MYEOF
 Types: deb
 URIs: https://download.docker.com/linux/ubuntu
 Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
 Components: stable
 Signed-By: /etc/apt/keyrings/docker.asc
-EOF
+MYEOF
 
 apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
